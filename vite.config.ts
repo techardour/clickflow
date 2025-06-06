4647b0c3cbaec96337e2fcbfb6e7bae399f9661b
Jignesh Patel <105312026+techardour@users.noreply.github.com>
2025-06-06 15:28:26 +0530

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, // Listen on all network interfaces
+    port: 5173,
     proxy: {
       '/auth/api': {
         target: 'http://test1.fundly.ai',
